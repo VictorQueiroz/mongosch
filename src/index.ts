34b717a2e81e8c29cd7c70b6e958821b27310fa6
Victor Queiroz <victorcqueirozg@gmail.com>
2023-10-04 16:42:35 -0300

    export `merge` and `FieldTypeBinary`

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
+export { FieldTypeBinary } from "./schema/FieldTypeBinary";
 export { FieldTypeDate } from "./schema/FieldTypeDate";
 export { FieldTypeEnumString, FieldTypeEnumInt } from "./schema/FieldTypeEnum";
 export {
@@ -12,3 +13,4 @@ export { Field } from "./schema/Field";
 export { FieldType } from "./schema/FieldType";
 export { FieldTypeBoolean } from "./schema/FieldTypeBoolean";
 export { Model } from "./schema/Model";
+export { default as merge } from './merge';
